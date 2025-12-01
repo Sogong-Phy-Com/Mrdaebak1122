dbea47f91c7312ae8cdfc987dadd1cff9205e9c3
DavJang <pandol826@gmail.com>
2025-12-01 10:35:39 +0900

    chore: ?ъ슜?섏? ?딅뒗 import ?쒓굅

--- server-java/src/main/java/com/mrdabak/dinnerservice/controller/VoiceOrderController.java
+++ server-java/src/main/java/com/mrdabak/dinnerservice/controller/VoiceOrderController.java
@@ -18,13 +18,11 @@ import com.mrdabak.dinnerservice.voice.service.VoiceOrderCheckoutService;
 import com.mrdabak.dinnerservice.voice.service.VoiceOrderSessionService;
 import com.mrdabak.dinnerservice.voice.service.VoiceOrderSummaryMapper;
 import jakarta.validation.Valid;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
